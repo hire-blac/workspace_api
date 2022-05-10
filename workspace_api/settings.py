4ec3f292c063b6ee1863a29b1de87113a8cbf21e
danihire@gmail.com <danihire@gmail.com>
2022-05-10 14:21:50 +0100

    dotenv

--- workspace_api/settings.py
+++ workspace_api/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-jat^^!xj=!+$tuk9rrg0z0%!gdsrqz6uu%z)6)bl0hclrwcaw*'
-
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
