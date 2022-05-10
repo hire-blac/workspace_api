d9790b4ffdfb44e174c11600425c0d48e620ca93
danihire@gmail.com <danihire@gmail.com>
2022-05-10 14:31:47 +0100

    allowed hosts

--- workspace_api/settings.py
+++ workspace_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['workspace-api3342.herokuapp.com', '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
