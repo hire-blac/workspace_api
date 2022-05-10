6652e102a8dae6bc34d6a7c21784d0805e6571e9
danihire@gmail.com <danihire@gmail.com>
2022-05-10 13:29:57 +0100

    configs

--- workspace_api/settings.py
+++ workspace_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jat^^!xj=!+$tuk9rrg0z0%!gdsrqz6uu%z)6)bl0hclrwcaw*'
+# SECRET_KEY = 'django-insecure-jat^^!xj=!+$tuk9rrg0z0%!gdsrqz6uu%z)6)bl0hclrwcaw*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
